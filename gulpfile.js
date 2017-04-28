f3a7377c304516e5a9858835e236fd6a5639cf21
fenwick67 <smileman66@gmail.com>
2017-04-27 21:58:08 -0500

    working

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ var pug         = require('gulp-pug');
 var serve       = require('gulp-serve');
 var locals      = require('./locals.js');
 var browserSync = require('browser-sync').create();
-
-
+var rename      = require('gulp-rename');
+var insert      = require('gulp-insert');
+var replace     = require('gulp-replace');
 // Save a reference to the `reload` method
 
 // Watch scss AND html files, doing different things with each.
@@ -24,15 +25,35 @@ gulp.task('watch-static',function(){
   gulp.watch('*.pug', ['pug']);
 });
 
-gulp.task('srcInject',function(){
+/*
+  This magical function takes .src files and injects the local js vars into it.
+*/
+gulp.task('injectLocals',function(){
   // inject my locals into src files
   var toInject = 'var locals = '+JSON.stringify(locals) + ';\r\n\r\n';
-  return gulp.src('*.src')
-  .pipe(function(){
-    
-  })
-  .pipe(gulp.dest("./"));
+  return gulp.src('./*.src')
+    .pipe(insert.prepend(toInject))
+    .pipe(rename(function(path){
+      // remove the .src
+      path.extname = path.extname.replace(/\.src/i,'')
+    }))
+    .pipe(gulp.dest("./"));
+});
 
+/*
+  This magical function takes ebook html files and
+  injects the reader javascript
+*/
+gulp.task('addEbookScripts',function(){
+  var replacer = '<body><script src="/reader.js"></script>';
+  var re = /<body>/i;
+  return gulp.src('./books/html/*.addreader')
+    .pipe(replace(re,replacer))
+    .pipe(rename(function(path){
+      // remove the .addreader
+      path.extname = path.extname.replace(/\.addreader/i,'')
+    }))
+    .pipe(gulp.dest("./books/html/"));
 });
 
 gulp.task('pug', function buildHTML() {
@@ -54,7 +75,7 @@ gulp.task('watch-bs', function() {
   gulp.watch('*.pug', ['pug','reload']);
 });
 
-gulp.task('default',['pug','watch-static','serve-static']);
-gulp.task('dev',['watch-bs','pug','serve-bs','reload'])
+gulp.task('default',['addEbookScripts','injectLocals','pug','watch-static','serve-static']);
+gulp.task('dev',['addEbookScripts','injectLocals','watch-bs','pug','serve-bs','reload'])
 
 // note that adding the "reader" script is not done here, but manually.
